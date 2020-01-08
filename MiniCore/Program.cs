201e822dda825019fdbcad15a32b77dd7f59615c
徐健 <jian.xu@1hai.cn>
2020-01-08 11:03:58 +0800

    Seconde Push

--- MiniCore/Program.cs
+++ MiniCore/Program.cs
@@ -10,29 +10,29 @@ namespace MiniCore
     {
         static void Main(string[] args)
         {
-            //var dictKeys = new Dictionary<string, string>()
-            //    {
-            //        {"OrderId","订单号"},
-            //        {"OrderStatus","订单状态"},
-            //        {"OrderPrice","订单总金额"},
-            //        {"OrderPayAmount","支付金额"},
-            //        {"OrderArrearsPrice","欠费金额"},
-            //        {"BatchId","批次号"},
-            //        {"OprStatusDesc","处理状态"},
-            //        {"OprNo","操作人工号"},
-            //        {"OprName","操作人姓名"},
-            //        {"StrOprTime","操作时间"}
-            //    };//{"OrderId":"订单号","OrderStatus":"订单状态","OrderPrice":"订单总金额","OrderPayAmount":"支付金额","OrderArrearsPrice":"欠费金额","BatchId":"批次号","OprStatusDesc":"处理状态","OprNo":"操作人工号","OprName":"操作人姓名","StrOprTime":"操作时间"}
+            var dictKeys = new Dictionary<string, string>()
+                {
+                    {"OrderId","订单号"},
+                    {"OrderStatus","订单状态"},
+                    {"OrderPrice","订单总金额"},
+                    {"OrderPayAmount","支付金额"},
+                    {"OrderArrearsPrice","欠费金额"},
+                    {"BatchId","批次号"},
+                    {"OprStatusDesc","处理状态"},
+                    {"OprNo","操作人工号"},
+                    {"OprName","操作人姓名"},
+                    {"StrOprTime","操作时间"}
+                };//{"OrderId":"订单号","OrderStatus":"订单状态","OrderPrice":"订单总金额","OrderPayAmount":"支付金额","OrderArrearsPrice":"欠费金额","BatchId":"批次号","OprStatusDesc":"处理状态","OprNo":"操作人工号","OprName":"操作人姓名","StrOprTime":"操作时间"}
 
-            //var serialDict = JsonConvert.SerializeObject(dictKeys);
-            //Console.WriteLine(serialDict);
-            //var DeserialDict = JsonConvert.DeserializeObject<Dictionary<string, string>>(serialDict);
-            //foreach (var item in DeserialDict)
-            //{
-            //    Console.WriteLine(JsonConvert.SerializeObject(item));
-            //}
-            //var dt = DealTradeDate(Convert.ToDateTime("2020-01-01 0:0:00"), Convert.ToDateTime("2020-01-01"));
-            //Console.WriteLine(JsonConvert.SerializeObject(dt));
+            var serialDict = JsonConvert.SerializeObject(dictKeys);
+            Console.WriteLine(serialDict);
+            var DeserialDict = JsonConvert.DeserializeObject<Dictionary<string, string>>(serialDict);
+            foreach (var item in DeserialDict)
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(item));
+            }
+            var dt = DealTradeDate(Convert.ToDateTime("2020-01-01 0:0:00"), Convert.ToDateTime("2020-01-01"));
+            Console.WriteLine(JsonConvert.SerializeObject(dt));
 
             System.Diagnostics.Process.Start("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe", "http://demomanage.1hai.cn/OrderInfo/Index?phoneNo=13585885928");
             Console.ReadLine();
